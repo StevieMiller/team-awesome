fc84221782fb62304e85ed02c73adfe221564eaf
StevieMiller <stephanie.miller@carlsbadschools.net>
2023-02-09 07:55:57 -0700

    still can't figure out how to transfer employee data

--- src/genHTML.js
+++ src/genHTML.js
@@ -1,11 +1,42 @@
-function genHTML(employees) {
-  return `<h1>hello world</h1>
-  <p>Name: ${employees[0].name}<p>
-  <p>Role: ${employees[0].getRole()}<p>
-  <p>ID: ${employees[0].id}<p>
-  <p>Email: ${employees[0].email}<p>
-  <p>Office Number: ${employees[0].officeNum}<p>
-  `
+
+
+const genHTML = (team) => {
+  console.log(team);
+
+// Function to hold all meta data at the top of the page
+// Two big functions
+// This (genHTML) will be the layout
+// ${genTEAM(team)} <-- this is how I will pull the team data into the template literal to be rendered to the HTML page
+
+
+
+const genTEAM = () => {
+// Create a function for each card. Ex: genEMP, genENG, genINT
+// Generate a card that brings in data
+// One for each role
 }
+
+const genMAN = () => {
+
+}
+
+const genENG = () => {
+
+}
+
+const genINT = () => {
+
+}
+}
+
+// function genHTML(employees) {
+//   return `<h1>hello world</h1>
+//   <p>Name: ${employees[0].name}<p>
+//   <p>Role: ${employees[0].getRole()}<p>
+//   <p>ID: ${employees[0].id}<p>
+//   <p>Email: ${employees[0].email}<p>
+//   <p>Office Number: ${employees[0].officeNum}<p>
+//   `
+// }
 
 module.exports = genHTML;
